91e8d301e5ce1fbd662e3512d213436313e1f708
dederico <dederico@gmail.com>
2020-09-22 20:29:33 -0500

    metodo para agregar un elemento a una lista

--- 3_Kata_ComputerScience/5_Estructuras_De_Datos/linked_list.js
+++ 3_Kata_ComputerScience/5_Estructuras_De_Datos/linked_list.js
@@ -1,3 +1,12 @@
+// El nodo es la informacion del elemento y su apuntador(o enlace) al siguiente elemento.
+class Node {
+  constructor(data) {
+    // en data guardamos cualquier objeto que queramos
+    this.data = data;
+    // es quien nos indica el siguiente nodo, por defecto este es nulo.
+    this.next = null;
+  }
+}
 // Estructura de datos
 // Una manera de trabajar con los datos
 
@@ -5,4 +14,21 @@
 // una lista ligada debe tener una cabecera (HEAD)
 // Nos va a indicar el inicio de la lista.
 
-// El tamano de la lista (tambien se va a guardar)
+// El tamano de la lista (length) (tambien se va a guardar)
+
+// Las listas tienen 2 comportamientos escenciales,
+// insertar (add)
+// borrar (delete)
+class LinkedList {
+  // estas son las propiedades de mi lista
+  constructor() {
+    this.head = null;
+    this.length = 0;
+  }
+  // estos son los metodos o comportamientos de mi lista
+  add(data) {
+    const node = new Node(data); // {data: data, next:null}
+    this.head = node;
+    this.length++;
+  }
+}
