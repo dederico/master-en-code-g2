3279cfd04d7b6287922b12cfbd0eb2f356bdb908
dederico <dederico@gmail.com>
2020-09-22 22:02:29 -0500

    method add, and delete

--- 3_Kata_ComputerScience/5_Estructuras_De_Datos/linked_list.js
+++ 3_Kata_ComputerScience/5_Estructuras_De_Datos/linked_list.js
@@ -25,10 +25,69 @@ class LinkedList {
     this.head = null;
     this.length = 0;
   }
+  isEmpty() {
+    return this.head === null;
+  }
   // estos son los metodos o comportamientos de mi lista
-  add(data) {
+  addAtEnd(data) {
+    console.log("add " + data);
     const node = new Node(data); // {data: data, next:null}
-    this.head = node;
+    if (this.head === null) {
+      this.head = node;
+    } else {
+      let currentNode = this.head;
+      // while: mientras (condicion: se cumpla la condicion) {hace esto}
+      while (currentNode.next) {
+        // verifico si next tiene un nodo
+        console.log(data);
+        console.log(currentNode);
+        currentNode = currentNode.next; // sustitutyo el nodo que vive en next, a mi nodo actual
+      }
+      // cuando next sea nulo, entonces sale del while y continua con el resto del codigo
+      currentNode.next = node;
+    }
+
     this.length++;
   }
+  addAtStart() {}
+
+  get(index) {
+    // 0
+    if (this.head === null || index > this.length) {
+      return null;
+    } else {
+      // 1 -> 2 -> 3
+      let counter = 1;
+      let currentNode = this.head;
+      while (counter !== index) {
+        counter++;
+        currentNode = currentNode.next;
+      }
+      return currentNode;
+    }
+  }
+
+  delete(data) {
+    let currentNode = this.head;
+    let previousNode = null;
+    if (currentNode.data === data) {
+      this.head = this.currentNode.next;
+    } else {
+      while (currentNode.data !== data && currentNode.next !== null) {
+        previousNode = currentNode;
+        currentNode = currentNode.next;
+      }
+      previousNode.next = currentNode.next;
+      this.length--;
+    }
+  }
 }
+const listaDeAlumnos = new LinkedList();
+
+listaDeAlumnos.add("Cesar");
+listaDeAlumnos.add("Juan Camilo");
+listaDeAlumnos.add("Marlon");
+listaDeAlumnos.add("Ulma");
+listaDeAlumnos.add("Margarito");
+
+console.log(listaDeAlumnos);
