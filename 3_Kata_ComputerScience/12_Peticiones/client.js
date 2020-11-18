e0907fed6be4c41b94154cbe9284069f1f71012d
dederico <dederico@gmail.com>
2020-11-17 21:21:16 -0600

    peticiones

---
+++ 3_Kata_ComputerScience/12_Peticiones/client.js
@@ -0,0 +1,7 @@
+const request = require("request");
+console.log("Empezando peticion");
+request.get("https://www.google.com", (err, response, body) => {
+  console.log(response.statusCode);
+  console.log(body);
+});
+console.log("Termino la peticion");
