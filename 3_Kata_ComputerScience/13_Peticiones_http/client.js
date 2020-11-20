89f63df5f3ada987ad2da2552243e78108d370d3
dederico <dederico@gmail.com>
2020-11-19 19:08:43 -0600

    tarea

--- 3_Kata_ComputerScience/13_Peticiones_http/client.js
+++ 3_Kata_ComputerScience/13_Peticiones_http/client.js
@@ -1,6 +1,19 @@
 const request = require("request");
-console.log("Empezando peticion");
-request.get("https://www.google.com", (err, response, body) => {
-  console.log(response.statusCode);
-});
+
+const getPokemonName = (name) => {
+  request.get(
+    `https://pokeapi.co/api/v2/pokemon/${name}`,
+    (err, response, body) => {
+      if (response.statusCode === 200) {
+        const json = JSON.parse(body);
+        console.log(json.name);
+      } else {
+        console.log("Ocurrio un error");
+      }
+    }
+  );
+};
+
 console.log("Termino la peticion");
+
+getPokemonName("pikachu");
