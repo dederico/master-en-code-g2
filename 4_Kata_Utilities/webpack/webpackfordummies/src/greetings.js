4e06fc300031ff846deb5767b5f06c1c5b76b3fb
dederico <dederico@gmail.com>
2020-11-25 21:21:13 -0600

    clase miercoles

---
+++ 4_Kata_Utilities/webpack/webpackfordummies/src/greetings.js
@@ -0,0 +1,13 @@
+const greeting = (name) =>
+  `Hola ${name} este es un saludo, bienvenido a ECMA6.`;
+
+module.exports = greeting;
+
+// const valorObtenidoDelArchivo = require('ruta/del/archivo')
+// common JS
+
+// Forma que lee BABEL
+export default greeting;
+
+// Forma que lee NODE
+//module.exports = greeting;
---
+++ 4_Kata_Utilities/webpack/webpackfordummies/src/index.js
@@ -0,0 +1,6 @@
+// commonJS
+//const greeting = require("./greetings");
+// NODE
+import greeting from "./greetings";
+
+console.log(greeting("Generacion-2"));
