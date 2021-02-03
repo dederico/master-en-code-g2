63e737fc4fc1fe0e08d6192b0ee3181df02b7e3a
dederico <dederico@gmail.com>
2021-02-02 20:38:40 -0600

    repaso React

---
+++ 5_Kata_FrontEnd/todo-app/src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import './App.css';
+import HolaMundo from './components/HolaMundo'
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Todo App      <HolaMundo />
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
