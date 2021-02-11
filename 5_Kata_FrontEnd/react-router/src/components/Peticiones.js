9e336e0587a6d29004662d4c6702cf5fa57baae8
dederico <dederico@gmail.com>
2021-02-10 20:23:43 -0600

    Agregar peticion a tacofancyAPI usando useEffect y axios

--- 5_Kata_FrontEnd/react-router/src/components/Peticiones.js
+++ 5_Kata_FrontEnd/react-router/src/components/Peticiones.js
@@ -1,8 +1,9 @@
+import axios from 'axios'
 import { useState, useEffect } from 'react';
 
 
 export default function Peticiones() {
-    const [texto, setTexto] = useState("");
+    const [taco, setTaco] = useState({});
     /*
     useEffect (escrito asi) se ejecuta siempre
     despues de renderizar (retornar) nuestro componente
@@ -18,19 +19,42 @@ export default function Peticiones() {
     esto es el equivalente a 
     componentDidMount
     */
-    useEffect(
-        () => {
-            console.log("Hola desde el useEffect!!!")
-        }, [])
+
+    const tacoAPI = 'http://taco-randomizer.herokuapp.com/random/';
+
+    useEffect(() => {
+        // Hacer la peticion a la API
+        axios.get(tacoAPI)
+            .then((response) => {
+                setTaco(response.data)
+            })
+
+            .catch((err) => console.log(err));
+        // Con la respuesta, modificar el estado de mi componente 
+    }, [])
+
+
+    // useEffect(
+    //     () => {
+    //         console.log("Hola desde el useEffect!!!")
+    //     }, [])
     return (
         <div className="container">
-            <h2>Peticiones</h2>
-            <h3>useEffect</h3>
-            <input
+            <h1>Peticiones</h1>
+            <h2>useEffect</h2>
+            <h3>- Taco Fancy API -</h3>
+            {/* <input
                 type="texto"
                 onChange={(e) => setTexto(e.target.value)}></input>
             <br />
-            <span>{texto}</span>
+            <span>{texto}</span> */}
+            <p>{
+
+                taco.condiment
+                    ? taco.condiment.name
+                    : 'Estamos eligiendo tu taco'
+
+            }</p>
         </div>
     )
 }
