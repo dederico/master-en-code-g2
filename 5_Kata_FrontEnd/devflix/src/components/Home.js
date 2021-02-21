688dcd2fc9ad5eee8169cfa87148a8faba1142f5
dederico <dederico@gmail.com>
2021-02-21 07:47:04 -0600

    mostrar data de shows

--- 5_Kata_FrontEnd/devflix/src/components/Home.js
+++ 5_Kata_FrontEnd/devflix/src/components/Home.js
@@ -3,8 +3,32 @@ import PropTypes from 'prop-types';
 
 const Home = ({ shows }) => {
     console.log('En Home:', shows);
+    // shows.show
+    // show.name
+    // show.image.medium
+    // show.summary
+    const showResults = () => {
+        return shows.map(showObject => {
+            return (<h3>{showObject.show.name}</h3>)
+        });
+
+
+
+    }
     return (
-        <h1>Home</h1>
+        <>
+            {
+                shows.length > 0
+                    ?
+                    showResults()
+                    : <h1>Home</h1>
+
+            }
+            <card>
+
+            </card>
+
+        </>
     )
 }
 
