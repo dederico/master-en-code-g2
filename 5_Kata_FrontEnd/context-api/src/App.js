b78e4183fbdf7ccf10e59f795f41e60b8123f998
dederico <dederico@gmail.com>
2021-03-08 20:39:27 -0600

    hover y click logic

--- 5_Kata_FrontEnd/context-api/src/App.js
+++ 5_Kata_FrontEnd/context-api/src/App.js
@@ -1,10 +1,12 @@
 import ClickCounter from './components/ClickCounter'
+import HoverCounter from './components/HoverCounter'
 
 function App() {
   return (
     <>
       <h1>Higher Order Components</h1>
       <ClickCounter />
+      <HoverCounter />
     </>
   );
 }
