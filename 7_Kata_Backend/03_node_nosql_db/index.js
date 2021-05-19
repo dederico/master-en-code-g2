2a8ffbc37235f0706e274b9776ee4d2eff53c0d4
Federico Gonzalez <dederico@gmail.com>
2021-05-18 20:01:26 -0500

    repaso API

---
+++ 7_Kata_Backend/03_node_nosql_db/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express(); // Aplicaion de API 
+const PORT = process.env.PORT || 4020;
+
+app.get('/prueba', (req, res) => {
+    res.status(200).json({ message: 'success' })
+});
+
+app.listen(PORT, () => console.log("It's alive!"));
+
