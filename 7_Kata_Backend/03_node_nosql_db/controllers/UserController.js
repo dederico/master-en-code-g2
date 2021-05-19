64ba61115fde01a939a20a35cd0075841652c234
Federico Gonzalez <dederico@gmail.com>
2021-05-18 20:26:46 -0500

    agregar modelo encargado de comunicarse con la bd

--- 7_Kata_Backend/03_node_nosql_db/controllers/UserController.js
+++ 7_Kata_Backend/03_node_nosql_db/controllers/UserController.js
@@ -1,5 +1,9 @@
+const User = require('.,/models/User')
+
+
 module.exports = {
     create: (req, res) => {
+       // User.create(req.body)
     res.status(201).json({ message: 'user created successfully' });
 },
     findAll: (req, res) => {
