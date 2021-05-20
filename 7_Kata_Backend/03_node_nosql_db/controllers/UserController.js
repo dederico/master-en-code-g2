1ef8b26611f1f391df756999bb9b96e0c970b596
Federico Gonzalez <dederico@gmail.com>
2021-05-19 20:37:21 -0500

    agregar endpoint a agregar usuario y update

--- 7_Kata_Backend/03_node_nosql_db/controllers/UserController.js
+++ 7_Kata_Backend/03_node_nosql_db/controllers/UserController.js
@@ -12,16 +12,36 @@ module.exports = {
             const newUser = User.create(req.body);
             res.status(201).json({ message: 'user created successfully' });
         } catch (error) {
-             res.status(400).json({ message: 'error creating users' });
+            res.status(400).json({ message: 'error creating users' });
         }
-},
+    },
     findAll: async (req, res) => {
         try {
             const users = await User.find();
-                res.status(200).json({ message: 'user list obtained', users });
+            res.status(200).json({ message: 'user list obtained', users });
         } catch (error) {
-                            res.status(400).json({ message: 'error fetching users', error });
+            res.status(400).json({ message: 'error fetching users', error });
 
         }
-},
-}
+    },
+    findOne: async (req, res) => {
+            try {
+                const idUser = req.params.idUser;
+                const user = await User.findById(idUser);
+                return res.status(200).json({ message: 'user found', user })
+            }
+            catch (error) {
+                return res.status(500).json({ error });
+            }
+    },
+    update: async (req, res) => {
+        try {
+            const idUser = req.params.idUser;
+            const user = await User.findByIdAndUpdate(idUser, req.body, { new: true });
+            return res.status(200).json({ message: 'user updated', user: updateUser })
+        } catch (error) {
+            return res.status(500).json({ error})
+        }
+        },
+    }
+
--- 7_Kata_Backend/03_node_nosql_db/routers/UserRouter.js
+++ 7_Kata_Backend/03_node_nosql_db/routers/UserRouter.js
@@ -2,7 +2,17 @@ const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/UserController')
 
+// Create
 router.post('/users', UserController.create);
+
+//Read(All)
 router.get('/users', UserController.findAll);
+
+// Read(One)
+router.get('/users/:idUser', UserController.findOne);
+
+// Update (One)
+// PUT -> VAMOS A REEMPLAZAR TODO EL OBJETO
+router.patch('/users/:idUser', UserController.update)
 
 module.exports = router;
